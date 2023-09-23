4d8b729da9e586167750be2e707729f912417174
Yan Sidyakin <sidyakinian@gmail.com>
2023-09-23 16:54:00 -0400

    Copy refactoring changes to multi-node broadcast and add random selection function

--- multi-node-broadcast/main.go
+++ multi-node-broadcast/main.go
@@ -3,51 +3,75 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 type MessageBody struct {
     Type    string `json:"type"`
-    Message int    `json:"message"`
+    Element int    `json:"message"`
+}
+
+type server struct {
+	node *maelstrom.Node
+	elements []int
 }
 
 func main() {
-	n := maelstrom.NewNode()
-	// create an array to store messages
-	var messages []int
+	node := maelstrom.NewNode()
+	server := &server{node: node, elements: []int{}}
 	
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body MessageBody
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-	
-		messages = append(messages, body.Message)
-	
-		// Echo the original message back with the updated message type.
-		response_body := map[string]string{
-			"type": "broadcast_ok",
-		}
-		return n.Reply(msg, response_body)
-	})
+	node.Handle("broadcast", server.handleBroadcast)
+	node.Handle("read", server.handleRead)
+	node.Handle("topology", server.handleTopology)
 
-	n.Handle("read", func(msg maelstrom.Message) error {
-		response_body := map[string]any{
-			"type": "read_ok",
-			"messages": messages,
-		}
-		return n.Reply(msg, response_body)
-	})
-
-	n.Handle("topology", func (msg maelstrom.Message) error {
-		response_body := map[string]any{
-			"type": "topology_ok",
-		}
-		return n.Reply(msg, response_body)
-	})
-
-	if err := n.Run(); err != nil {
+	if err := node.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func (server *server) handleBroadcast(message maelstrom.Message) error {
+	var body MessageBody
+	if err := json.Unmarshal(message.Body, &body); err != nil {
+		return err
+	}
+
+	server.elements = append(server.elements, body.Element)
+
+	response_body := map[string]string{
+		"type": "broadcast_ok",
+	}
+	return server.node.Reply(message, response_body)
+}
+
+func (server *server) handleRead(message maelstrom.Message) error {
+	response_body := map[string]any{
+		"type": "read_ok",
+		"messages": server.elements,
+	}
+	return server.node.Reply(message, response_body)
+}
+
+func (server *server) handleTopology(message maelstrom.Message) error {
+	response_body := map[string]any{
+		"type": "topology_ok",
+	}
+	return server.node.Reply(message, response_body)
+}
+
+func randomSelection(strings []string, fraction float64) []string {
+	copyOfStrings := append([]string(nil), strings...)
+
+	rand.Shuffle(len(copyOfStrings), func(i, j int) {
+		copyOfStrings[i], copyOfStrings[j] = copyOfStrings[j], copyOfStrings[i]
+	})
+
+	n := int(float64(len(copyOfStrings)) * fraction)
+
+	if n < 1 {
+		n = 1
+	}
+
+	return copyOfStrings[:n]
 }
